b2af4654bf191e34d2dff317a8547d78b990b069
NicholasKhatch <nicholas.khatchadourian23@ycdsbk12.ca>
2021-12-14 11:57:30 -0500

    final version 1, added random variable and current time

--- Sketch.java
+++ Sketch.java
@@ -2,13 +2,16 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
-	
+  float fltRandomX = random(0, width);
+  float fltRandomY = random(0, height);
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
   public void settings() {
 	// put your size call here
     size(400, 400);
+
   }
 
   /** 
@@ -16,20 +19,38 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(204, 255, 249);
+
+    fill(0,0,0);
+    textSize(15);
+    text(month() + "|" + day() + "|" + year() + " - " + hour() + ":" + minute() + ":" + second(), 265, 330);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    stroke(35, 92, 35);
+    line(200 - fltRandomX,240 - fltRandomY,240,260);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    stroke(35, 92, 35);
+    line(150 - fltRandomX,220 - fltRandomY,200,200);
+
+    stroke(35, 92, 35);
+    fill(60, 179, 45);
+    rect(180 - fltRandomX, 200 - fltRandomY, 40, 150);
+
+    stroke(0, 0, 0);
+    fill(255,0,0);
+    ellipse(200 - fltRandomX, 150 - fltRandomY,150,150);
+
+    stroke(0, 0, 0);
+    fill(0,0,0);
+    ellipse(200 - fltRandomX, 150 - fltRandomY,65,65);
+
+    stroke(0, 0, 0);
+    fill(63, 122, 55);
+    rect(0,350,400,50); 
   }
   
   // define other methods down here.
